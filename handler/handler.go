a51920d69fc157012a7e949e8148b1f14c6d7fb1
jt <1293628336@qq.com>
2022-07-29 13:47:15 +0800

    添加文件元信息

--- handler/handler.go
+++ handler/handler.go
@@ -6,6 +6,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/jyyds/filestore/meta"
+	"github.com/jyyds/filestore/util"
+	"golang.org/x/tools/godoc/util"
 )
 
 // 处理文件上传
@@ -28,19 +33,28 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "./tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02-15:04:05"),
+		}
+
 		os.Mkdir("./tmp", os.ModeDir)
-		newFile, err := os.Create("./tmp/" + head.Filename)
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Printf("Failed to create file,err=%s\n", err.Error())
 			return
 		}
 		defer newFile.Close()
 
-		_, err = io.Copy(newFile, file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			fmt.Printf("Failed to save data into file,err=%s\n", err.Error())
 			return
 		}
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
 
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
