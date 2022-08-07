6c49c10e1b404100541f89406e338e13e05cf53b
jt <1293628336@qq.com>
2022-08-07 10:07:14 +0800

    添加文件分块上传和断点续传

--- go.mod
+++ go.mod
@@ -3,3 +3,13 @@ module github.com/jyyds/filestore
 go 1.18
 
 require github.com/go-sql-driver/mysql v1.6.0
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
+
+require (
+	github.com/gomodule/redigo v1.8.9 // indirect
+	github.com/json-iterator/go v1.1.12
+)
--- main.go
+++ main.go
@@ -16,6 +16,12 @@ func main() {
 	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
 	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(handler.TryFastUploadHandler))
 
+	// 分块上传接口
+	http.HandleFunc("/file/mpupload/init", handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
+	http.HandleFunc("/file/mpupload/uppart", handler.HTTPInterceptor(handler.UploadPartHandler))
+	http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))
+
+	// 用户相关接口
 	http.HandleFunc("/user/signup", handler.SignupHandler)
 	http.HandleFunc("/user/signin", handler.SignInHandler)
 	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
