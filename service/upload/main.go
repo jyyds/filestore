c7f835925c1551084de401a277df2f58bd673da0
jt <1293628336@qq.com>
2022-08-23 11:50:59 +0800

    使用Gin改造接口

--- service/upload/main.go
+++ service/upload/main.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/jyyds/filestore/handler"
+	cfg "github.com/jyyds/filestore/config"
+	"github.com/jyyds/filestore/route"
 )
 
 func main() {
-	http.HandleFunc("/file/upload", handler.UploadHandler)
-	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
-	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
-	http.HandleFunc("/file/download", handler.DownloadHandler)
-	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
-	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
-	// 秒传接口
-	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(handler.TryFastUploadHandler))
 
-	// 下载oss上文件的url
-	http.HandleFunc("/file/downloadurl", handler.HTTPInterceptor(handler.DownloadURLHandler))
-
-	// 分块上传接口
-	http.HandleFunc("/file/mpupload/init", handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
-	http.HandleFunc("/file/mpupload/uppart", handler.HTTPInterceptor(handler.UploadPartHandler))
-	http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))
-
-	// 用户相关接口
-	http.HandleFunc("/user/signup", handler.SignupHandler)
-	http.HandleFunc("/user/signin", handler.SignInHandler)
-	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
-	http.HandleFunc("/user/home", handler.HomeHandler)
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Printf("Filed to start server,err = %s", err.Error())
-	}
+	router := route.Router()
+	router.Run(cfg.UploadServiceHost)
 }
